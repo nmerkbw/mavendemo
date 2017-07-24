714b1edf14214dacbec586590789fa20b6f29f99
louyuting <1849491904@qq.com>
2017-07-24 20:48:40 +0800

    “

--- git.sh
+++ git.sh
@@ -5,8 +5,8 @@ echo "git add -A"
 git add -A
 
 echo "git commit"
-echo "请输入commit的注释信息：$1"
-git commit -m “$1”
+read -p "请输入commit的注释信息:" comment
+git commit -m “$comment”
 
 echo "git fetch origin master"
 git fetch origin master
@@ -16,3 +16,7 @@ git merge origin/master
 
 echo "git push origin master:master"
 git push origin master:master
+
+
+
+sleep 5s #睡眠5s
