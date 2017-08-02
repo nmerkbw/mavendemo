6ce0072c045b16595d2835ce23227dce0c971d1d
chris.lyt <chris.lyt@cainiao.com>
2017-08-02 17:45:56 +0800

    一次change

--- git.sh
+++ git.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 echo "开始提交代码到github"
-echo "git add -A"
-git add -A
+echo "git add ."
+git add .
 
 echo "git commit"
-read -p "请输入commit的注释信息:" comment
-git commit -m “$comment”
+echo "请输入commit的注释信息:"
+comment="commit new code"
+read comment
+git commit -m $comment
 
 echo "git fetch origin master"
 git fetch origin master
@@ -17,4 +19,3 @@ git merge origin/master
 echo "git push origin master:master"
 git push origin master:master
 
-sleep 5s #睡眠5s
