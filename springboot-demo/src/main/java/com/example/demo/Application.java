7cbffd4a6441d4a9eb0d616613fd6e323888b161
惜暮 <chris.lyt@cainiao.com>
2017-09-18 09:07:15 +0800

    commot

--- springboot-demo/src/main/java/com/example/demo/Application.java
+++ springboot-demo/src/main/java/com/example/demo/Application.java
@@ -5,13 +5,20 @@ import org.apache.ibatis.annotations.Mapper;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.ApplicationContext;
 
 @Slf4j
 @SpringBootApplication(scanBasePackages = "com.example.demo")
 // 设置Mybatis的Mapper扫描目标包
 @MapperScan(basePackages = "com.example.demo.mapper", annotationClass = Mapper.class)
+@EnableCaching
 public class Application {
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+		ApplicationContext applicationContext = SpringApplication.run(Application.class, args);
+
+		Object Object = applicationContext.getBean("cacheManager");
+        System.out.println(Object.toString());
+
+    }
 }
